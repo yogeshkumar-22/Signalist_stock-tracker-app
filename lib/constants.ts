76f52c805f0d054ef2f36b980bc7f94c7be8a6b7
yogesh kumar <ayogeshkumar22@gmail.com>
2026-04-25 16:39:57 +0530

    implment header

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const NAV_ITEMS:{href:string; label:string}[] = [
+    { href: '/', label: 'Dashboard'},
+    { href: '/search', label: 'Search'},
+    { href: '/watchlist', label: 'Watchlist'},
+]
